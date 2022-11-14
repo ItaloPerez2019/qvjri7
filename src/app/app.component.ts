8c92e016dadd351ba406d67013c625e47bacbc16
ItaloPerez2019 <43344909+ItaloPerez2019@users.noreply.github.com>
2022-11-13 19:57:57 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 import { TeamService } from './team.service';
 import { SearchCriteria } from './SearchCriteria';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -11,8 +12,9 @@ export class AppComponent {
   results = [];
   leagues = [];
   showCriteria = true;
+  searchForm: FormGroup;
 
-  constructor(private teamService: TeamService) {}
+  constructor(private teamService: TeamService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.teamService.getAllTeams().subscribe((teams) => {
@@ -22,6 +24,49 @@ export class AppComponent {
       this.leagues = leagues;
       this.leagues.unshift({ id: -1, name: 'Any' });
     });
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.searchForm = this.fb.group({
+      teamName: '',
+      leagueId: -1,
+      gameNightsMonday: false,
+      gameNightsWednesday: false,
+      gameNightsThursday: false,
+      gameNightsfriday: false,
+      gameNightsSaturday: false,
+    });
+  }
+
+  onSearch() {
+    let criteria = new SearchCriteria();
+    let teamName = this.searchForm.value.teamName.trim();
+    criteria.name = teamName === '' ? null : teamName;
+
+    criteria.leagueId =
+      this.searchForm.value.leagueId === -1
+        ? null
+        : this.searchForm.value.leagueId;
+
+    criteria.gameNights = this.getGameNightsCriteria();
+
+    console.log(this.searchForm.value.leagueId);
+
+    this.teamService.searchTeams(criteria).subscribe((teams) => {
+      console.log('search');
+      this.results = teams;
+    });
+  }
+
+  getGameNightsCriteria() {
+    let gameNights = [];
+    if (this.searchForm.value.gameNightsMonday) gameNights.push('M');
+    if (this.searchForm.value.gameNightsWednesday) gameNights.push('W');
+    if (this.searchForm.value.gameNightsThursday) gameNights.push('H');
+    if (this.searchForm.value.gameNightsFriday) gameNights.push('F');
+    if (this.searchForm.value.gameNightsSaturday) gameNights.push('S');
+    return gameNights.join('');
   }
 
   toggleSearchCriteria() {
